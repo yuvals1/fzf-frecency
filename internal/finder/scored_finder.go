d61122f1a1406f3b732dee0e6b5a1d518ac83993
Yuval Spiegel <yuvalspiegel1@gmail.com>
2025-01-26 00:32:47 +0200

    feat: add sorting

--- internal/finder/scored_finder.go
+++ internal/finder/scored_finder.go
@@ -2,6 +2,7 @@ package finder
 
 import (
     "fmt"
+    "sort"
 
     "github.com/yuvals1/fzf-frecency/internal/frecency"
     "github.com/yuvals1/fzf-frecency/internal/pathutil"
@@ -11,6 +12,20 @@ import (
 type ScoredFile struct {
     Path  string
     Score int
+}
+
+// ScoredFiles is a slice of ScoredFile that can be sorted
+type ScoredFiles []ScoredFile
+
+func (sf ScoredFiles) Len() int      { return len(sf) }
+func (sf ScoredFiles) Swap(i, j int) { sf[i], sf[j] = sf[j], sf[i] }
+func (sf ScoredFiles) Less(i, j int) bool {
+    // Sort by score (highest first)
+    if sf[i].Score != sf[j].Score {
+        return sf[i].Score > sf[j].Score
+    }
+    // Then by path (alphabetically)
+    return sf[i].Path < sf[j].Path
 }
 
 // ScoredFinder combines file finding with frecency scoring
@@ -31,35 +46,45 @@ func NewScoredFinder(scorer *frecency.FrecencyScore, normalizer *pathutil.PathNo
 
 // FindScoredFiles returns a channel of files with their frecency scores
 func (sf *ScoredFinder) FindScoredFiles(root string) (<-chan ScoredFile, error) {
-    // Create output channel for scored files
-    scoredFiles := make(chan ScoredFile)
-    
-    // Get raw files channel
+    // Get all files first
     files, err := sf.FindFiles(root)
     if err != nil {
         return nil, fmt.Errorf("finding files: %w", err)
     }
 
-    // Process files and add scores
-    go func() {
-        defer close(scoredFiles)
-        
-        for path := range files {
-            // Normalize the path for scoring
-            normalizedPath, err := sf.normalizer.NormalizePath(path)
-            if err != nil {
-                fmt.Printf("Warning: couldn't normalize path %s: %v\n", path, err)
-                continue
-            }
+    // Collect and score all files
+    var scoredFiles ScoredFiles
 
-            // Get score and send scored file
-            score := sf.scorer.GetScore(normalizedPath)
-            scoredFiles <- ScoredFile{
-                Path:  path,
-                Score: score,
-            }
+    // Process files
+    for path := range files {
+        // Normalize the path for scoring
+        normalizedPath, err := sf.normalizer.NormalizePath(path)
+        if err != nil {
+            fmt.Printf("Warning: couldn't normalize path %s: %v\n", path, err)
+            continue
+        }
+
+        // Get score and add to slice
+        score := sf.scorer.GetScore(normalizedPath)
+        scoredFiles = append(scoredFiles, ScoredFile{
+            Path:  path,
+            Score: score,
+        })
+    }
+
+    // Sort the files
+    sort.Sort(scoredFiles)
+
+    // Create output channel
+    out := make(chan ScoredFile)
+
+    // Send sorted files through channel
+    go func() {
+        defer close(out)
+        for _, file := range scoredFiles {
+            out <- file
         }
     }()
 
-    return scoredFiles, nil
+    return out, nil
 }
